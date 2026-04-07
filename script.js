4d80c4b4573cf3018b2adb0af75469a8f34ffd03
Fdli Habibi Lubis <habibilubisf@gmail.com>
2026-04-07 23:44:16 +0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-            function saklar (params) {
+function saklar(action, lamp) {
+  let toogle1 = document.getElementById("default-toogle1");
+  let toogle2 = document.getElementById("default-toogle2");
+  let toogle3 = document.getElementById("default-toogle3");
 
-                let lampu1 = document.getElementById("lampu1");
+  let lampu1 = document.getElementById("lampu1");
+  let lampu2 = document.getElementById("lampu2");
+  let lampu3 = document.getElementById("lampu3");
 
-                if (params == "on") {
-                    lampu1.src = "Assets/images/on.png"
-                } 
-                if (params == "off") {
-                    lampu1.src = "Assets/images/off.png"
-                } 
-                return lampu1;
-            }
+  if (toogle1.checked) {
+    lampu1.src = "Assets/images/on.png";
+  } else {
+    lampu1.src = "Assets/images/off.png";
+  }
+  if (toogle2.checked) {
+    lampu2.src = "Assets/images/on.png";
+  } else {
+    lampu2.src = "Assets/images/off.png";
+  }
+  if (toogle3.checked) {
+    lampu3.src = "Assets/images/on.png";
+  } else {
+    lampu3.src = "Assets/images/off.png";
+  }
+}
