9f0084e19805e01e03aff4000405960b3739b773
Fdli Habibi Lubis <habibilubisf@gmail.com>
2026-04-09 00:07:43 +0700

    update

--- script.js
+++ script.js
@@ -1,25 +1,93 @@
-function saklar(action, lamp) {
-  let toogle1 = document.getElementById("default-toogle1");
-  let toogle2 = document.getElementById("default-toogle2");
-  let toogle3 = document.getElementById("default-toogle3");
+function saklar() {
+  let t1 = document.getElementById("t1");
+  let t2 = document.getElementById("t2");
+  let t3 = document.getElementById("t3");
+  let t4 = document.getElementById("t4");
+  let t5 = document.getElementById("t5");
+  let t6 = document.getElementById("t6");
+  let t7 = document.getElementById("t7");
+  let t8 = document.getElementById("t8");
+  let t9 = document.getElementById("t9");
+  let t10 = document.getElementById("t10");
 
   let lampu1 = document.getElementById("lampu1");
   let lampu2 = document.getElementById("lampu2");
   let lampu3 = document.getElementById("lampu3");
+  let lampu4 = document.getElementById("lampu4");
+  let lampu5 = document.getElementById("lampu5");
+  let lampu6 = document.getElementById("lampu6");
+  let lampu7 = document.getElementById("lampu7");
+  let lampu8 = document.getElementById("lampu8");
+  let lampu9 = document.getElementById("lampu9");
+  let lampu10 = document.getElementById("lampu10");
 
-  if (toogle1.checked) {
+  // Lampu 1
+  if (t1.checked) {
     lampu1.src = "Assets/images/on.png";
   } else {
     lampu1.src = "Assets/images/off.png";
   }
-  if (toogle2.checked) {
+
+  // Lampu 2
+  if (t2.checked) {
     lampu2.src = "Assets/images/on.png";
   } else {
     lampu2.src = "Assets/images/off.png";
   }
-  if (toogle3.checked) {
+
+  // Lampu 3
+  if (t3.checked) {
     lampu3.src = "Assets/images/on.png";
   } else {
     lampu3.src = "Assets/images/off.png";
+  }
+
+  // Lampu 4
+  if (t4.checked) {
+    lampu4.src = "Assets/images/on.png";
+  } else {
+    lampu4.src = "Assets/images/off.png";
+  }
+
+  // Lampu 5
+  if (t5.checked) {
+    lampu5.src = "Assets/images/on.png";
+  } else {
+    lampu5.src = "Assets/images/off.png";
+  }
+
+  // Lampu 6
+  if (t6.checked) {
+    lampu6.src = "Assets/images/on.png";
+  } else {
+    lampu6.src = "Assets/images/off.png";
+  }
+
+  // Lampu 7
+  if (t7.checked) {
+    lampu7.src = "Assets/images/on.png";
+  } else {
+    lampu7.src = "Assets/images/off.png";
+  }
+
+  // Lampu 8
+  if (t8.checked) {
+    lampu8.src = "Assets/images/on.png";
+  } else {
+    lampu8.src = "Assets/images/off.png";
+  }
+
+  // Lampu 9
+  if (t9.checked) {
+    lampu9.src = "Assets/images/on.png";
+  } else {
+    lampu9.src = "Assets/images/off.png";
+  }
+
+  // Lampu 10
+  if (t10.checked) {
+    lampu10.src = "Assets/images/on.png";
+  } else {
+    lampu10.src = "Assets/images/off.png";
   }
 }
